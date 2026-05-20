f9035a9a1e9ee434cca776858c104265a0d3ea4b
Jawaan Santh <jawaan25fcrit@gmail.com>
2026-05-20 15:07:16 +0530

    Fix PWA missing asset config build error

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.png', 'favicon.svg'],
       manifest: {
         name: 'FocusFlow',
         short_name: 'FocusFlow',
